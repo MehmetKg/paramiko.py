a6e6977f3d2ef15aaa58573bd511683c7a3c0472
Mehmet Karagülle <117034396+MehmetKg@users.noreply.github.com>
2025-03-09 22:30:34 +0300

    Add files via upload
    Metasploitable gibi bir hedef sisteme SSH üzerinden bağlanarak testler yapmak.
     Sunuculara uzaktan bağlanıp komut çalıştırmak.
     Siber güvenlik testleri yapmak (örneğin, pentest süreçlerinde).

---
+++ paramiko.py
@@ -0,0 +1,34 @@
+import paramiko
+
+
+def sendCommand(ssh, command):
+    stdin, stdout, stderr = ssh.exec_command(command)
+    cmd_output = stdout.read()
+    print(">>", cmd_output.decode())
+
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+ip = '127.0.0.1'  # Kendi makinen
+port = 22 # SSH portu
+username = 'kendi_kullanıcı_adın'
+password = 'kendi_şifren'
+
+
+try:
+    ssh.connect(ip, port=port, username=username, password=password)
+    print(f"[+] Bağlantı başarılı: {ip}")
+
+    command = input(">> ")
+    while command.lower().strip() != "quit":
+        if command.strip():
+            sendCommand(ssh, command)
+        command = input(">> ")
+
+except Exception as e:
+    print(f"[-] Hata oluştu: {e}")
+
+finally:
+    ssh.close()
+    print("[+] Bağlantı kapatıldı.")
